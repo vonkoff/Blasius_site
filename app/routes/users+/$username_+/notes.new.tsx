2a52966c9e3840d9d8ffb758bc176bb672486461
Ivan Vukusic <jaaajaa102@gmail.com>
2024-01-06 14:07:26 -0500

    First commit

---
+++ app/routes/users+/$username_+/notes.new.tsx
@@ -0,0 +1,11 @@
+import { json, type DataFunctionArgs } from '@remix-run/node'
+import { requireUserId } from '#app/utils/auth.server.ts'
+import { NoteEditor, action } from './__note-editor.tsx'
+
+export async function loader({ request }: DataFunctionArgs) {
+	await requireUserId(request)
+	return json({})
+}
+
+export { action }
+export default NoteEditor
